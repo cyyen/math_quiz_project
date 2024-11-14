d3d08041f498d8a3c0b5c8d23f9b7f18ba0fed66
CY Yen <yenchihya@gmail.com>
2024-11-14 15:01:51 +0800

    feat: commit

--- math_quiz/settings.py
+++ math_quiz/settings.py
@@ -86,10 +86,21 @@ WSGI_APPLICATION = "math_quiz.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres.qsslpppfqdsiskargpnn",
+        "PASSWORD": "sNI13p9OLjApDy7c",
+        "HOST": "aws-0-ap-southeast-1.pooler.supabase.com",
+        "PORT": "6543",
     }
 }
 
